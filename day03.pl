9efddcec0370cde22bd09773d6282bedfe2ed2b2
Thilankavishka <thilankavishka25@gmail.com>
2024-10-18 01:49:18 +0530

    day03 part2

--- day03.pl
+++ day03.pl
@@ -20,3 +20,27 @@ cal(C):-write('Enter first integer:'),read(X),nl,
          write('Enter second integer:'),read(Y),nl,
          write('Enter your operation:'),read(N),
          ans(X,Y,N,C).
+
+%5 output integers from first to last
+output(Last,Last) :- write(Last),nl,write('End of Example'),nl.
+output(First,Last) :- First=\=Last,write(First),nl,N is First+1,output(N,Last).
+
+
+%6 sum the integers from 1 to N
+sum(1,1).
+sum(N,S) :- N>1,N1 is N-1,sum(N1,S1),S = S1+N,write(S),nl.
+
+%7)output first N squares, one per line
+writesquares(1):-write(1),nl.
+writesquares(N):-N>1,N1 is N-1,writesquares(N1),Nsq is N*N,write(Nsq),nl.
+
+
+%8)read terms entered by the user from the keyboard and output them to the screen until  end is encountered
+go:-loop(start).
+loop(end).
+loop(X):-X\=end,write('Type end to end:'),read(W),write('Input was:'),write(W),nl,loop(W).
+
+
+%9)find factorial of a positive integer N
+fact(1,1).
+fact(N,F):- N>1,N1 is N - 1,fact(N1,F1),F is F1*N.
